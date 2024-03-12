ff9782010dda03dc1a4c62964b773264bc38ff39
yenit6 <144127970+yenit6@users.noreply.github.com>
2024-03-12 10:51:20 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main (String[]angs){
+     String nome = "sexo e catatau  ";
+
+double x = -2   ;
+   
+   System.out.println("a): " +(4+3*x+5*x-10-x));
+   
+   System.out.println("b): " +(5*x - 4*x+ 10*x - 10*x));
+   
+   System.out.println("c): "+(10*x-20*x+10/x+5*x-3*x));
+
+    }
+}
---
+++ catatau.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main (String[]angs){
+     String nome = "sexo e catatau  ";
+   
+   float n1 = 10;
+   float n2 = 20;
+   
+   float soma = n1 + n2;
+   float sub  = n2 - n1;
+   float mult = n1 * n2;
+   float div = n2 / n1;
+   
+     
+        System.out.println(nome);
+        System.out.println(soma);
+        System.out.println(sub);
+        System.out.println(mult);
+        System.out.println(div);
+    }
+}
