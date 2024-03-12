39c65fe357a8257335d8ea39b12c18a960b810ab
yenit6 <144127970+yenit6@users.noreply.github.com>
2024-03-12 11:04:33 -0300

    Add files via upload

---
+++ nomes.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	
+     String nome = "sexo e catatau  ";
+     
+   String[] verdura = {"\nmakos", "\ncachorro", "\nneymar"};
+   
+   System.out.println(verdura[2]);
+	}
+}
