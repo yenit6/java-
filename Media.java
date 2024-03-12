c3571cb6739c6500ff3473b874a792373a481165
yenit6 <144127970+yenit6@users.noreply.github.com>
2024-03-12 11:09:22 -0300

    Add files via upload

---
+++ Media.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		float n1 = 10;
+		float n2 = 5;
+		float n3 = 8;
+		float media = (n1 + n2 + n3) / 3;
+	
+	    System.out.println(media);
+	    
+	}
+}
